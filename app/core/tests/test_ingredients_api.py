628f50d43cee1346c075ca08cb624c101d41e512
emzubair <muhammad.zubair@arbisoft.com>
2021-07-01 20:58:21 +0500

    create test and endponit added

--- app/core/tests/test_ingredients_api.py
+++ app/core/tests/test_ingredients_api.py
@@ -62,3 +62,21 @@ class PrivateIngredientsAPITest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0].get('name'), ingred.name)
+
+    def test_create_ingredent_successful(self):
+        """Test ingredient is created successfull"""
+
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user, name=payload.get('name')).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test ingredient with invalid data is not crated"""
+
+        res = self.client.post(INGREDIENTS_URL, {'name': ''})
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
