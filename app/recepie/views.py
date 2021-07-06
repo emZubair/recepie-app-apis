d0bbc9771a20db3f5f29f43744b1d98ea3bedfcd
emzubair <muhammad.zubair@arbisoft.com>
2021-07-06 12:08:54 +0500

    Recepie update & create with tests

--- app/recepie/views.py
+++ app/recepie/views.py
@@ -51,3 +51,6 @@ class RecepieViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return RecepieDetailSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
